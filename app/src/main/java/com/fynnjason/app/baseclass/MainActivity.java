9422122c95687be079aa0eda5420ce653117bf9b
fynnjason <410132905@qq.com>
2018-09-28 10:03:10 +0800

    1

--- app/src/main/java/com/fynnjason/app/baseclass/MainActivity.java
+++ app/src/main/java/com/fynnjason/app/baseclass/MainActivity.java
@@ -1,7 +1,5 @@
 package com.fynnjason.app.baseclass;
 
-import android.util.Log;
-
 import com.fynnjason.app.baseclass.basemvc.BaseMvcActivity;
 
 public class MainActivity extends BaseMvcActivity {
@@ -13,7 +11,7 @@ public class MainActivity extends BaseMvcActivity {
 
     @Override
     public void initData() {
-        Log.e("MainActivity", "initData: " + getActivity().getLocalClassName());
+
     }
 
     @Override
